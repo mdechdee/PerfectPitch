e4a6fd0f9d9775129c9bcb057c30ec7616b0a9cc
daclouds <daclouds@gmail.com>
2019-11-17 00:11:49 +0900

    Remove unused code

--- OldMusician/Node.gd
+++ OldMusician/Node.gd
@@ -91,7 +91,6 @@ func quiz():
 			a[p].play()
 	timer = get_node("Timer")
 	timer.set_wait_time(6)
-	timer.connect("timeout", self, "_on_Timer_timeout")
 	timer.start()
 
 func evaluation(answer):
@@ -101,19 +100,3 @@ func evaluation(answer):
 		if !(q in answer):
 			 return false
 	return true
-
-func _wait(seconds):
-	self._create_timer(self, seconds, true, "_emit_timer_end_signal")
-	yield(self,"timer_end")
-
-func _emit_timer_end_signal():
-	emit_signal("timer_end")
-
-func _create_timer(object_target, float_wait_time, bool_is_oneshot, string_function):
-	timer = Timer.new()
-	timer.set_one_shot(bool_is_oneshot)
-	timer.set_timer_process_mode(0)
-	timer.set_wait_time(float_wait_time)
-	timer.connect("timeout", object_target, string_function)
-	self.add_child(timer)
-	timer.start()
