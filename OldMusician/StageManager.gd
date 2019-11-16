d487d0ebed7a03258a0298184e23a1fc0596c199
mdechdee <dechdee.m@gmail.com>
2019-11-17 03:21:15 +0900

    Improve bpm time

--- OldMusician/StageManager.gd
+++ OldMusician/StageManager.gd
@@ -18,22 +18,9 @@ func _ready():
 	timer.connect("timeout", self, "_on_Timer_timeout")
 	quiz()
 	timer.start()
-
+	$BGStreamPlayer.play(5)
+	
 func _process(delta):
-	if (timer.get_time_left() < 1.0):
-		if evaluation(answer):
-			$Score.text = str(int($Score.text) + 1)
-		$Label.text = str(0)
-		timer.stop()
-		
-		$CorrectAnswer.text = str('Correct Answer: ', quiz)
-		$YourAnswer.text = str('Your Answer: ', answer)
-		
-		quiz()
-		timer = get_node("Timer")
-		timer.set_wait_time(two_bar_sec)
-		#timer.connect("timeout", self, "_on_Timer_timeout")
-		timer.start()
 	if (timer.get_time_left() >= 0):
 		$Label.text = str(floor(timer.get_time_left()))
 
@@ -41,14 +28,14 @@ func quiz():
 	quiz = []
 	answer = []
 	var SM = $SpritesManager
-	for note in SM.note_to_sprite.keys():
-		SM.note_to_sprite[note].set_position(Vector2(SM.note_to_sprite[note].get_position().x, 0))
 	var b = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
 	for i in range(1):
 		var p = randi()%12
 		if !(b[p] in quiz):
 			quiz.append(b[p])
 			robot_note_player.play_note(b[p])
+	for note in SM.note_to_sprite.keys():
+		SM.note_to_sprite[note].set_position(Vector2(SM.note_to_sprite[note].get_position().x, 0))
 	print(quiz)
 
 func evaluation(answer):
@@ -62,3 +49,14 @@ func evaluation(answer):
 
 func _on_NotePlayer_note_played(note):
 	answer.append(note)
+
+
+func _on_Timer_timeout():
+	if evaluation(answer):
+		$Score.text = str(int($Score.text) + 1)
+	$Label.text = str(0)
+	$CorrectAnswer.text = str('Correct Answer: ', quiz)
+	$YourAnswer.text = str('Your Answer: ', answer)
+	quiz()
+	timer.set_wait_time(two_bar_sec)
+	timer.start()
