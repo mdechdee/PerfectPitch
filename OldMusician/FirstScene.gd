8daf7d2505979bb6b069a851da272d61c97e5133
daclouds <daclouds@gmail.com>
2019-11-16 21:22:27 +0900

    Add score

--- OldMusician/FirstScene.gd
+++ OldMusician/FirstScene.gd
@@ -4,9 +4,10 @@ onready var P = $AudioStreamPlayer
 onready var length = P.stream.get_length()
 onready var length_per_note = length/12
 
-var movement = Vector2(0, 2)
+var movement = Vector2(0, 5)
 var timer
 var notes = []
+var answer = []
 
 signal timer_end
 
@@ -40,6 +41,7 @@ func _input(event):
             if $C in notes:
                 return
             notes.append($C)
+            answer.append("C")
         elif event.is_action_pressed("C#"):
             P1.play(length_per_note)
         elif event.is_action_pressed("D"):
@@ -47,6 +49,7 @@ func _input(event):
             if $D in notes:
                 return
             notes.append($D)
+            answer.append("D")
         elif event.is_action_pressed("D#"):
             P1.play(length_per_note*3)
         elif event.is_action_pressed("E"):
@@ -54,17 +57,20 @@ func _input(event):
             if $E in notes:
                 return
             notes.append($E)
+            answer.append("E")
         elif event.is_action_pressed("F"):
             P1.play(length_per_note*5)
             if $F in notes:
                 return
             notes.append($F)
+            answer.append("F")
         elif event.is_action_pressed("F#"):
             P1.play(length_per_note*6)
         elif event.is_action_pressed("G"):
             if $G in notes:
                 return
             notes.append($G)
+            answer.append("G")
             P1.play(length_per_note*7)
         elif event.is_action_pressed("G#"):
             P1.play(length_per_note*8)
@@ -83,20 +89,24 @@ func _ready():
     quiz()
 
     timer = get_node("Timer")
-    timer.set_wait_time(11)
+    timer.set_wait_time(6)
     timer.connect("timeout", self, "_on_Timer_timeout")
     timer.start()
 
 func _process(delta):
     if (timer.get_time_left() < 1.0):
+        print(answer)
+        if "C" in answer and "E" in answer and "G" in answer:
+            $Score.text = str(int($Score.text) + 1)
         $Label.text = str(0)
         timer.stop()
         
         #_wait(10)
+        answer = []
         quiz()
         timer = get_node("Timer")
-        timer.set_wait_time(11)
-        timer.connect("timeout", self, "_on_Timer_timeout")
+        timer.set_wait_time(6)
+        #timer.connect("timeout", self, "_on_Timer_timeout")
         timer.start()
     if (timer.get_time_left() >= 0):
         $Label.text = str(floor(timer.get_time_left()))
